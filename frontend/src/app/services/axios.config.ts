491b9d2ae965ec31d07ec062e482a5cf119011b4
Z3r0J <tibboxcentral@gmail.com>
2023-06-05 03:32:57 -0400

    feat: docker-compose.yml to run the whole project feat: Dockerfile for frontend to build the image and run the angular project in a container feat: axios.config.ts to use the backend url port as the base url

--- frontend/src/app/services/axios.config.ts
+++ frontend/src/app/services/axios.config.ts
@@ -2,6 +2,6 @@ import { AxiosInstance } from './../../../node_modules/axios/index.d';
 import axios from 'axios';
 
 export const axiosConfig: AxiosInstance = axios.create({
-  baseURL: 'https://localhost:5001/api/v1',
-  headers: { 'X-Custom-Header': 'foobar' },
+  baseURL: 'http://localhost:5000/api/v1',
+  headers: { 'Content-Type': 'application/json' },
 });
